662141cb26e55ce1c74183e1c1017030f31c15de
Sascha Falk <sascha@falk-online.eu>
2020-05-06 07:42:30 +0200

    Update to new version of build system

--- Build.ps1
+++ Build.ps1
@@ -1,7 +1,12 @@
-param([switch]$buildserver)
-
 [String]$ScriptDir = Split-Path $Script:MyInvocation.MyCommand.Path
-Import-Module "$ScriptDir\build.vs\Build+Deploy.psm1"
+$ErrorActionPreference = "Stop"
+
+Import-Module "$ScriptDir\build.vs\GitVersion.psm1"
+Import-Module "$ScriptDir\build.vs\Clean.psm1"
+Import-Module "$ScriptDir\build.vs\RestoreNuGet.psm1"
+Import-Module "$ScriptDir\build.vs\PreBuildWizard.psm1"
+Import-Module "$ScriptDir\build.vs\Build.psm1"
+Import-Module "$ScriptDir\build.vs\LicenseCollector.psm1"
 
 # -------------------------------------------------------------------------------------------------------------------------------------
 
@@ -9,29 +14,50 @@ Import-Module "$ScriptDir\build.vs\Build+Deploy.psm1"
 # configuration
 # #################################
 
-$SolutionPath = "$ScriptDir\PreBuildWizard.sln"
-$MsbuildConfigurations = @('Release')
+[String]  $ProductName          = "PreBuildWizard"
+[String]  $SolutionPath         = "$ScriptDir\PreBuildWizard.sln"
+[String]  $LicenseTemplatePath  = "$ScriptDir\THIRD_PARTY_NOTICES.template"
+[String]  $BuildOutputPath      = "$ScriptDir\_build\PreBuildWizard"
+
+$MsbuildConfigurations = @('Debug','Release')
 $MsbuildPlatforms = @('Any CPU')
 
 # -------------------------------------------------------------------------------------------------------------------------------------
 
-# build all projects
-if ($buildserver)
-{
-	Build `
-		-SolutionPath "$SolutionPath" `
-		-MsbuildConfigurations $MsbuildConfigurations `
-		-MsbuildPlatforms $MsbuildPlatforms
-}
-else
-{
-	Build `
-		-SolutionPath "$SolutionPath" `
-		-MsbuildConfigurations $MsbuildConfigurations `
-		-MsbuildPlatforms $MsbuildPlatforms `
-		-PauseOnError
+# remove old build output
+Clean `
+	-OutputPaths @("$ScriptDir\_build", "$ScriptDir\_deploy") `
+	-PauseOnError
 
-	Read-Host "Press ANY key..."
-}
+# let GitVersion determine the version number from the git history via tags
+GitVersion -PauseOnError
 
+# restore NuGet packages
+RestoreNuGet `
+	-SolutionPath "$SolutionPath" `
+	-PauseOnError
 
+# patch templates and assembly infos with current version number
+# check consistency of NuGet packages
+PreBuildWizard `
+	-SolutionPath "$SolutionPath" `
+	-PauseOnError
+
+# build projects
+Build `
+	-SolutionPath "$SolutionPath" `
+	-MsbuildConfigurations $MsbuildConfigurations `
+	-MsbuildPlatforms $MsbuildPlatforms `
+	-SkipConsistencyCheck `
+	-PauseOnError
+
+# collect license for release build
+LicenseCollector `
+	-SolutionPath "$SolutionPath" `
+	-Configuration "Release" `
+	-Platform "Any CPU" `
+	-TemplatePath "$LicenseTemplatePath" `
+	-OutputPath "$ScriptDir\_build\PreBuildWizard.Any CPU.Release\THIRD_PARTY_NOTICES" `
+	-PauseOnError
+
+Read-Host "Press ANY key..."
