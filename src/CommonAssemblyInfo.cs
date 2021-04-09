889aaa8dc1f81dee20df32a0906b06be04833a48
Sebastian Piel <sebastianpiel@outlook.de>
2021-04-09 10:39:37 +0200

    Update to newest version of build.vs
     - add version attributes to CommonAssemblyInfo
       - no more changes inside build.vs submodule

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -3,3 +3,13 @@
 [assembly: AssemblyCompany("Griffin+")]
 [assembly: AssemblyProduct("Griffin+ PreBuildWizard")]
 [assembly: AssemblyCopyright("Copyright (c) Sascha Falk, Sebastian Piel 2021")]
+
+[assembly: AssemblyVersionAttribute("1.0.0.0")];
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")];
+[assembly: AssemblyInformationalVersionAttribute("1.0.0")];
+
+#if DEBUG
+[assembly: AssemblyConfigurationAttribute("Debug")];
+#else
+[assembly: AssemblyConfigurationAttribute("Release")];
+#endif
